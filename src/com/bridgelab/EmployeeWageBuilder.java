835d6bcd776f5c839471023007edac051cfd2354
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 18:00:51 +0530

    MODIFIED: implemented daily wage calculation part, if employee is full time present then calculating daily wage and printing the output.

--- src/com/bridgelab/EmployeeWageBuilder.java
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -10,16 +10,23 @@ public class EmployeeWageBuilder
 	public static void main(String[] args)
 	{
 		int EMP_FULLTIME_PRESENT = 1;
+		int WAGE_PER_HR = 20;
+		int DAILY_WORKING_HRS = 8;
 		
-		/*Generating random number using math.random(),
+		int employeeDailyWage = 0;
+		
+		/*
+		 *Generating random number using math.random(),
 		 *Converting it to nearest possible integer number using Math.floor(),
 		 *By doing % 2 we will only get either number 0 or 1.  
-		 */
+		*/
 		int employeeChceck =(int) Math.floor(Math.random() * 10) % 2;
 		
 		if(EMP_FULLTIME_PRESENT == employeeChceck)
 		{
 			System.out.println("Employee is full time present.");
+			employeeDailyWage = WAGE_PER_HR * DAILY_WORKING_HRS;
+			System.out.println("Daily wage of an full timer employee is : " + employeeDailyWage + " Rs.");
 		}
 		else
 		{
