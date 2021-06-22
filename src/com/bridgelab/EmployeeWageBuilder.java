17e0108408093e4c22f82b8fc4c7945a701b4642
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 17:36:48 +0530

    ADDED: EmployeeWageBuilder java file which is initialy printing welcome message.

---
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -0,0 +1,14 @@
+package com.bridgelab;
+
+/**
+ * @author mihir
+ * With respect to employees attendance,
+ * Calculating employee daily and monthly wage.
+ */
+public class EmployeeWageBuilder
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to employee wage computation problem.");
+	}
+}
