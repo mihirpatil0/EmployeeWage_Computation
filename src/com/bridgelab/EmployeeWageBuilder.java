6721a999706c827a67fb3d51981b8322df487753
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 17:49:05 +0530

    MODIFIED: With the help of random number checking if employee is present or absent.

--- src/com/bridgelab/EmployeeWageBuilder.java
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -9,6 +9,21 @@ public class EmployeeWageBuilder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage computation problem.");
+		int EMP_FULLTIME_PRESENT = 1;
+		
+		/*Generating random number using math.random(),
+		 *Converting it to nearest possible integer number using Math.floor(),
+		 *By doing % 2 we will only get either number 0 or 1.  
+		 */
+		int employeeChceck =(int) Math.floor(Math.random() * 10) % 2;
+		
+		if(EMP_FULLTIME_PRESENT == employeeChceck)
+		{
+			System.out.println("Employee is full time present.");
+		}
+		else
+		{
+			System.out.println("Employee is absent.");
+		}
 	}
 }
