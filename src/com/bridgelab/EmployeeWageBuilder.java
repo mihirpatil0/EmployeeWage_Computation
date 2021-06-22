913521c8e6e4c050046ec93ebb12d34a7184e951
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 18:10:56 +0530

    ADDED: Employee part time present part, and calculating daily wage accordingly.

--- src/com/bridgelab/EmployeeWageBuilder.java
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -6,30 +6,42 @@ package com.bridgelab;
  * Calculating employee daily and monthly wage.
  */
 public class EmployeeWageBuilder
-{
+{	
+	static int EMP_FULLTIME_PRESENT = 2;
+	static int EMP_PARTTIME_PRESENT = 1;
+	static int WAGE_PER_HR = 20;
+	static int DAILY_WORKING_HRS = 8;
+	
+	static int employeeDailyWage = 0;
+	static int workingHrs = 0;
+	
 	public static void main(String[] args)
 	{
-		int EMP_FULLTIME_PRESENT = 1;
-		int WAGE_PER_HR = 20;
-		int DAILY_WORKING_HRS = 8;
-		
-		int employeeDailyWage = 0;
-		
 		/*
 		 *Generating random number using math.random(),
 		 *Converting it to nearest possible integer number using Math.floor(),
-		 *By doing % 2 we will only get either number 0 or 1.  
+		 *By doing % 3 we will only get either number 0 , 1 or 2.  
 		*/
-		int employeeChceck =(int) Math.floor(Math.random() * 10) % 2;
+		int employeeChceck =(int) Math.floor(Math.random() * 10) % 3;
+		
 		
 		if(EMP_FULLTIME_PRESENT == employeeChceck)
-		{
+		{	
+			workingHrs = 8;
 			System.out.println("Employee is full time present.");
 			employeeDailyWage = WAGE_PER_HR * DAILY_WORKING_HRS;
 			System.out.println("Daily wage of an full timer employee is : " + employeeDailyWage + " Rs.");
 		}
-		else
+		else if (EMP_PARTTIME_PRESENT == employeeChceck)
 		{
+			workingHrs = 4;
+			System.out.println("Employee is part time present.");
+			employeeDailyWage = WAGE_PER_HR * workingHrs;
+			System.out.println("Daily wage of an part timer employee is : " + employeeDailyWage + " Rs.");
+		}
+		else
+		{	
+			workingHrs = 0;
 			System.out.println("Employee is absent.");
 		}
 	}
