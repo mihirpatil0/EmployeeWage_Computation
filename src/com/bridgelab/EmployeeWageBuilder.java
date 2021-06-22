8bcc0d88dd9c87313bca4a10bd4eee501b5b2afc
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 18:29:06 +0530

    ADDED: Calculating monthly wage for an employee according to employess 20 days attendance.

--- src/com/bridgelab/EmployeeWageBuilder.java
+++ src/com/bridgelab/EmployeeWageBuilder.java
@@ -10,39 +10,47 @@ public class EmployeeWageBuilder
 	final static int EMP_FULLTIME_PRESENT = 2;
 	final static int EMP_PARTTIME_PRESENT = 1;
 	final static int WAGE_PER_HR = 20;
+	final static int MONTHLY_WORKING_DAY = 20;
 	
 	static int employeeDailyWage = 0;
 	static int workingHrs = 0;
+	static int employeeMonthlyWage = 0;
 	
 	public static void main(String[] args)
 	{
-		/*
-		 *Generating random number using math.random(),
-		 *Converting it to nearest possible integer number using Math.floor(),
-		 *By doing % 3 we will only get either number 0 , 1 or 2.  
-		*/
-		int employeeChceck =(int) Math.floor(Math.random() * 10) % 3;
-		
-		switch (employeeChceck)
+		for(int day = 1; day <= MONTHLY_WORKING_DAY; day++)
 		{
-		case EMP_FULLTIME_PRESENT:
-		{
-			workingHrs = 8;
-			System.out.println("Employee is full time present.");
-			employeeDailyWage = WAGE_PER_HR * workingHrs;
-			System.out.println("Daily wage of an full timer employee is : " + employeeDailyWage + " Rs.");
-			break;
+			/*
+			 *Generating random number using math.random(),
+			 *Converting it to nearest possible integer number using Math.floor(),
+			 *By doing % 3 we will only get either number 0 , 1 or 2.  
+			*/
+			int employeeChceck =(int) Math.floor(Math.random() * 10) % 3;
+			
+			switch (employeeChceck)
+			{
+			case EMP_FULLTIME_PRESENT:
+			{
+				workingHrs = 8;
+				System.out.println("Employee is full time present.");
+				employeeDailyWage = WAGE_PER_HR * workingHrs;
+				System.out.println("Daily wage of an full timer employee is : " + employeeDailyWage + " Rs.");
+				employeeMonthlyWage += employeeDailyWage;
+				break;
+			}
+			case EMP_PARTTIME_PRESENT:
+			{
+				workingHrs = 4;
+				System.out.println("Employee is part time present.");
+				employeeDailyWage = WAGE_PER_HR * workingHrs;
+				System.out.println("Daily wage of an part timer employee is : " + employeeDailyWage + " Rs.");
+				employeeMonthlyWage += employeeDailyWage;
+				break;
+			}
+			default:
+				System.out.println("Employee is absent.");
+			}
 		}
-		case EMP_PARTTIME_PRESENT:
-		{
-			workingHrs = 4;
-			System.out.println("Employee is part time present.");
-			employeeDailyWage = WAGE_PER_HR * workingHrs;
-			System.out.println("Daily wage of an part timer employee is : " + employeeDailyWage + " Rs.");
-			break;
-		}
-		default:
-			System.out.println("Employee is absent.");
-		}
+		System.out.println("\nEmployee monthly wage is : " + employeeMonthlyWage + " Rs.");
 	}
 }
